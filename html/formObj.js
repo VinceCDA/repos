661ba419f123232fecc69426d7e6f2da6741515c
VinceCDA <vincent.hitman47@hotmail.fr>
2021-09-02 18:20:49 +0200

    a

--- html/formObj.js
+++ html/formObj.js
@@ -1,20 +1,48 @@
 "use strict"
-$(function(){
-    $("#pseudo").on("keypress",function(event){
-        console.log($(event.target))
-        if ($(event.target).checkValidity()) {
-            
-            $(event.target).css("border-color","green")
+$(function () {
+    $( "input:text" ).css("border-width","thick")
+    $("#pseudo").on("keyup", function (event) {
+        if ($(event.target).valid()) {
+
+            $(event.target).css("border-color", "green")
+        }
+        else {
+            $(event.target).css("border-color", "red")
         }
     })
-    $("#password")
-    $("#confirm")
-    $("#mail")
+    $("#password").on("keyup", function (event) {
+        if ($(event.target).valid()) {
+
+            $(event.target).css("border-color", "green")
+        }
+        else {
+            $(event.target).css("border-color", "red")
+        }
+    })
+    $("#confirm").on("keyup", function (event) {
+        if ($(event.target).valid()) {
+
+            $(event.target).css("border-color", "green")
+        }
+        else {
+            $(event.target).css("border-color", "red")
+        }
+    })
+    $("#mail").on("keyup", function (event) {
+        if ($(event.target).valid()) {
+
+            $(event.target).css("border-color", "green")
+        }
+        else {
+            $(event.target).css("border-color", "red")
+        }
+    })
 })
+
 $("form").validate({
     debug: true,
-    rules:{
-        pseudo:"required",
-        password:"required",
+    rules: {
+        pseudo: "required",
+        password: "required",
     }
-  })
+})
