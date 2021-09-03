72f8ed3ae1d9434373fa15142265784a58413a2c
VinceCDA <vincent.hitman47@hotmail.fr>
2021-09-03 10:20:40 +0200

    p

--- html/formObj.js
+++ html/formObj.js
@@ -4,45 +4,60 @@ $(function () {
     $("#pseudo").on("keyup", function (event) {
         if ($(event.target).valid()) {
 
-            $(event.target).css("border-color", "green")
+            $(event.target).css("border-color", "green");
         }
         else {
-            $(event.target).css("border-color", "red")
+            $(event.target).css("border-color", "red");
         }
     })
     $("#password").on("keyup", function (event) {
         if ($(event.target).valid()) {
 
-            $(event.target).css("border-color", "green")
+            $(event.target).css("border-color", "green");
         }
         else {
-            $(event.target).css("border-color", "red")
+            $(event.target).css("border-color", "red");
         }
     })
     $("#confirm").on("keyup", function (event) {
         if ($(event.target).valid()) {
 
-            $(event.target).css("border-color", "green")
+            $(event.target).css("border-color", "green");
         }
         else {
-            $(event.target).css("border-color", "red")
+            $(event.target).css("border-color", "red");
         }
     })
     $("#mail").on("keyup", function (event) {
         if ($(event.target).valid()) {
 
-            $(event.target).css("border-color", "green")
+            $(event.target).css("border-color", "green");
         }
         else {
-            $(event.target).css("border-color", "red")
+            $(event.target).css("border-color", "red");
         }
+    })
+    $("#resetform").on("click",function(event){
+        $("form").validate().resetForm();
+        $( "input:text" ).css("border-color","")
     })
 })
 
 $("form").validate({
     debug: true,
     rules: {
-        pseudo: "required",
-        password: "required",
+        pseudo: {
+            required: true,
+            minlength: 5
+        },
+        password: {
+            minlength: 5
+        },
+        confirm: {
+            minlength: 5
+        },
+        mail:{
+
+        }
     }
 })
